b89347abc1112680aa1dc78dcf8e79a498ebb2a0
Nathan Kimutai <nathanpykimutai@gmail.com>
2019-10-07 02:30:35 +0300

    Fixing the error in the Null type exception in the Barcode intents

--- app/src/main/java/com/nathankimutai/adbms/BarcodeActivity.java
+++ app/src/main/java/com/nathankimutai/adbms/BarcodeActivity.java
@@ -2,21 +2,41 @@ package com.nathankimutai.adbms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class BarcodeActivity extends AppCompatActivity {
 
-    private Button mButton = (Button) findViewById(R.id.button);
-    private EditText useranameEditText = (EditText) findViewById(R.id.editText);
-    private EditText passwordEditText = (EditText) findViewById(R.id.editText2);
 
+
+    public void continueTo(View view){
+
+        Button mButton = (Button) findViewById(R.id.button);
+        EditText useranameEditText = (EditText) findViewById(R.id.editText);
+        EditText passwordEditText = (EditText) findViewById(R.id.editText2);
+        String username =useranameEditText.getText().toString();
+        Log.i("info",username);
+        if (username.equals("admin")){
+            Intent intent = new Intent(BarcodeActivity.this,Typesofbarcodes.class);
+            startActivity(intent);
+        }else {
+            Toast.makeText(this, "please use admin as the username", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode);
+
     }
 }
