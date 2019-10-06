c9cb630b765cc8842f72e4319e7ea723d5ea2e20
Nathan Kimutai <nathanpykimutai@gmail.com>
2019-10-07 00:32:56 +0300

    Adding the intent to the MainAcitivity.java so that the scan button can direct us to the Barcode Activity

--- app/src/main/java/com/nathankimutai/adbms/MainActivity.java
+++ app/src/main/java/com/nathankimutai/adbms/MainActivity.java
@@ -2,9 +2,21 @@ package com.nathankimutai.adbms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button scanbutton= (Button) findViewById(R.id.btn_scan);
+
+
+
+    public void scanMe(View view){
+        Intent intent = new Intent(MainActivity.this,BarcodeActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
