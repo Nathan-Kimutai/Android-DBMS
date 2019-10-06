9a07a35c7d4caef2533f1cb6ef12165d0786a10b
Nathan Kimutai <nathanpykimutai@gmail.com>
2019-10-07 01:40:17 +0300

    Changes in the BarcodeActivity.java

--- app/src/main/java/com/nathankimutai/adbms/BarcodeActivity.java
+++ app/src/main/java/com/nathankimutai/adbms/BarcodeActivity.java
@@ -3,8 +3,16 @@ package com.nathankimutai.adbms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BarcodeActivity extends AppCompatActivity {
+
+    private Button mButton = (Button) findViewById(R.id.button);
+    private EditText useranameEditText = (EditText) findViewById(R.id.editText);
+    private EditText passwordEditText = (EditText) findViewById(R.id.editText2);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
